186ebe87f54f3c3d460fba2d8e1f0d8678c7e313
zeyadsalah22 <112489529+zeyadsalah22@users.noreply.github.com>
2024-09-15 15:32:30 +0300

    dlete all

--- Day6Mydemo/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Day6Mydemo.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
